dcdc5ad8e672edad656dcafd8d8f81969d5fabbb
Jonathan Wright <devorakwright@gmail.com>
2022-01-06 14:11:33 +0200

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Accordion from './Components/Accordion';
+import Search from './Components/Search';
+import Dropdown from './Components/Dropdown';
+
+const items = [
+    {
+        title: 'What is REACT?',
+        content: 'REACT is a front-end framework'
+    },
+    {
+        title: 'Why use REACT',
+        content: 'REACT is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use REACT?',
+        content: 'We use REACT by creating components'
+    },
+];
+
+const options = [
+    {
+        label: 'The color red',
+        value: 'red'
+    },
+    {
+        label: 'The color green',
+        value: 'green'
+    },
+    {
+        label: 'A shade of blue',
+        value: 'blue'
+    }
+];
+
+export default () => {
+    return (
+        <div>
+            <Dropdown options={options} />
+        </div>
+    );
+};
